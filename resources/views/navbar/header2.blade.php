bbfb9eaf700f18654e3cd54c335a10fa40bcdee9
Samba Drame <dramembasa2000@gmail.com>
2023-07-06 13:11:10 +0000

    mise à jour

--- resources/views/navbar/header2.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="bg fixed-top">
-    <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container" style="justify-content: center">
-            <a class="navbar-brand" href="{{ route('home') }}">
-                <img src="{{ asset('assets/images/logos.png') }}" alt="">
-            </a>
-        </div>
-    </nav>
-</div>
