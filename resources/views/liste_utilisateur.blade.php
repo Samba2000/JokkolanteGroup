54dfbb2a6b39a92fcedc50f5e32b0e0d0776b851
Samba Drame <dramembasa2000@gmail.com>
2023-07-18 12:43:04 +0000

    mise à jour dashbord

--- resources/views/liste_utilisateur.blade.php
+++ resources/views/liste_utilisateur.blade.php
@@ -1,9 +1,28 @@
 @extends('dashbord')
-@section('title', 'Liste des utilisateurs')
+@section('head')
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Liste des utilisateurs</title>
+    <link rel="icon" href="{{ asset('assets/images/logo.ico') }}" type="image/x-icon" />
+    <link rel="stylesheet" href="{{ asset('assets/css/dashbord.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('assets/css/factures.css') }}"> --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/liste_utilisateur.css') }}">
+
+    <link href='https://fonts.googleapis.com/css?family=Manrope' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="ttps://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@700&family=Ubuntu&display=swap" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+@endsection
+{{-- @section('title', 'Liste des utilisateurs') --}}
 @section('content')
     <div class="liste_utlisateurs">
         <div class="col-sm-12 d-flex justify-content-center">
-            <h1 class="m-0">Bienvenue chez l’admin Jokkalante</h1>
+            <h1 class="">Bienvenue chez l’admin Jokkalante</h1>
         </div>
 
         <div class="table-responsive">
@@ -33,4 +52,11 @@
             </table>
         </div>
     </div>
+@endsection
+
+@section('foot')
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+    <script src="{{ asset('assets/js/pagination_dash.js') }}"></script>
 @endsection
